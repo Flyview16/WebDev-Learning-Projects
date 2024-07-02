5825e7f3c9fd3540873574320cea20b163f7fbc5
Stef-an-os <herbert.s.degadzor@gmail.com>
2024-07-02 03:28:50 +0000

    Building a calculator website on a server- handling post requests

--- Projects/14-My Express Server/server.js
+++ Projects/14-My Express Server/server.js
@@ -6,6 +6,14 @@ app.get("/", function(request, response){
     response.send("Hello World!");
 });
 
+app.get("/contact", (req, res) => {
+    res.send("Contact me at herbert@gmail.com");
+});
+
+app.get("/about", (req, res) => {
+    res.send("Full-stack developer");
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
