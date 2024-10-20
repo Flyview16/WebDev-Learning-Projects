babdb1fd516264d918f6a3d8bf3de914b55512d6
Stef-an-os <herbert.s.degadzor@gmail.com>
2024-10-20 16:06:13 +0000

    Creating note keeper app with react

---
+++ Projects/24-React-Keeper-App/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Note from './components/Note';
+
+function App() {
+  return (
+    <>
+      <div>
+      <Header />
+      <Note />
+      <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
